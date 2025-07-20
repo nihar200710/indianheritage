15dbf8d4aeaed3a812a8e518fdfd4382c70fbfad
nihar200710 <kollapudisainihar4@gmail>
2025-07-20 10:18:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div class name='dept'>
+     computer science and engineering <br></br>
+    <label classname='deptit'> information technology </label>
+    </div>
+  )
+}
+
+export default App
