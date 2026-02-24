9d9c5a4230f7835ad095bdaee425418ec8c85b55
nihar200710 <kollapudisainihar4@gmail>
2026-02-24 19:28:36 +0530

    Removed base for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   // This MUST match your GitHub repository name exactly!
-  base: '/indianheritage/' 
+ // base: '/indianheritage/' 
 })
