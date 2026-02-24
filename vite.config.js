5831dc58240c420f13df7109eca5888ca7f86bc1
nihar200710 <kollapudisainihar4@gmail>
2026-02-24 19:14:12 +0530

    Updated repo name and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   // Replace 'indian-heritage-frontend' with your EXACT GitHub repository name
-  base: '/indian-heritage-frontend/' 
+  base: '/indianheritage/' 
 })
